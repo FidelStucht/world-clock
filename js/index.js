94d9a829485f713e51df59c0cd4d0a574c41e1ff
FidelStucht <128236556+FidelStucht@users.noreply.github.com>
2023-05-24 23:55:50 +0200

    includes Sydney

--- js/index.js
+++ js/index.js
@@ -1,3 +1,43 @@
+function updateTime() {
+  // Los Angeles
+  let londonElement = document.querySelector("#london");
+  if (londonElement) {
+    let londonDateElement = londonElement.querySelector(".date");
+    let londonTimeElement = londonElement.querySelector(".time");
+    let londonTime = moment().tz("America/Los_Angeles");
+
+    londonDateElement.innerHTML = londonTime.format("MMMM	Do YYYY");
+    londonTimeElement.innerHTML = londonTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
+
+  // Munich
+  let munichElement = document.querySelector("#munich");
+  if (munichElement) {
+    let munichDateElement = munichElement.querySelector(".date");
+    let munichTimeElement = munichElement.querySelector(".time");
+    let munichTime = moment().tz("Europe/Paris");
+
+    munichDateElement.innerHTML = munichTime.format("MMMM	Do YYYY");
+    munichTimeElement.innerHTML = munichTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
+  // Sydney
+  let sydneyElement = document.querySelector("#sydney");
+  if (sydneyElement) {
+    let sydneyDateElement = sydneyElement.querySelector(".date");
+    let sydneyTimeElement = sydneyElement.querySelector(".time");
+    let sydneyTime = moment().tz("Australia/Sydney");
+
+    sydneyDateElement.innerHTML = sydneyTime.format("MMMM	Do YYYY");
+    sydneyTimeElement.innerHTML = sydneyTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
+}
+
 function updateCity(event) {
   let cityTimeZone = event.target.value;
   if (cityTimeZone === "current") {
@@ -18,6 +58,8 @@ function updateCity(event) {
         <a href="index.html">All cities</a>
         `;
 }
+updateTime();
+setInterval(updateTime, 1000);
 
 let citiesSelectElement = document.querySelector("#city");
 citiesSelectElement.addEventListener("change", updateCity);
